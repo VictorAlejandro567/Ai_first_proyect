3af90097ed232951b67e0a514608cfc69bf03e33
AlejandroRoberts <alejandro.roberts1@upr.edu>
2026-02-20 21:55:27 -0400

    primera parte proyecto.

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import time 
+import random
+# multiplicacion de 500 elementos. Se supone que no le añada al tiempo 
 
-first_array = [10,50,100,200,500]
-second_array = [10,50,100,200,500]
+first_array = [random.randint(1, 1000) for _ in range(500)]
+second_array = [random.randint(1,1000) for _ in range(500)]
 
 start = time.perf_counter()
 
-for i in range(5):
-    for j in range(5):
+for i in range(500):
+    for j in range(500):
         print(first_array[i] * second_array[j])
         
 end = time.perf_counter() 
